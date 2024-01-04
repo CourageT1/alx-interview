c90d5f469e51560cb53bee3da713e62bae8521a2
talent <124376251+CourageT1@users.noreply.github.com>
2024-01-04 19:35:53 +0000

    that one task

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,8 +1,7 @@
-#!/usr/bin/env python3
-"""
-a function def pascal_triangle(n)
-that returns a list of lists of integers representing the Pascal’s triangle of n:
-"""
+#!/usr/bin/python3
+"""a function def pascal_triangle(n)
+that returns a list of lists of integers representing
+the Pascal’s triangle of n:"""
 
 def pascal_triangle(n):
     """
