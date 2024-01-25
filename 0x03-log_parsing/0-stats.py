b921d90986051597a91bda32ea7291854e7945c2
talent <124376251+CourageT1@users.noreply.github.com>
2024-01-25 20:44:32 +0000

    correcting pycodestyle

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -6,7 +6,8 @@ line must be skipped)
 After every 10 lines and/or a keyboard interruption (CTRL + C), print these
 statistics from the beginning:
 Total file size: File size: <total size>
-where <total size> is the sum of all previous <file size> (see input format above)
+where <total size> is the sum of all previous <file size>
+(see input format above)
 Number of lines by status code:
 possible status code: 200, 301, 400, 401, 403, 404, 405 and 500
 if a status code doesn’t appear or is not an integer, don’t print anything
@@ -23,11 +24,13 @@ def print_statistics(total_size, status_counts):
 
     Args:
         total_size (int): Total file size.
-        status_counts (dict): Dictionary containing status codes and their counts.
+        status_counts (dict):
+        Dictionary containing status codes and their counts.
     """
     print("File size: {}".format(total_size))
     for status_code in sorted(status_counts):
         print("{}: {}".format(status_code, status_counts[status_code]))
+
 
 def parse_line(line):
     """
@@ -49,6 +52,7 @@ def parse_line(line):
     except (ValueError, IndexError):
         return None, None, None
 
+
 def main():
     """
     Main function to read stdin, compute statistics, and print results.
@@ -59,7 +63,11 @@ def main():
     try:
         for i, line in enumerate(sys.stdin, 1):
             ip_address, status_code, file_size = parse_line(line.strip())
-            if ip_address is not None and status_code is not None and file_size is not None:
+            if (
+                ip_address is not None
+                and status_code is not None
+                and file_size is not None
+            ):
                 total_size += file_size
                 status_counts[status_code] = status_counts.get(status_code, 0) + 1
 
@@ -70,6 +78,7 @@ def main():
         pass
     finally:
         print_statistics(total_size, status_counts)
+
 
 if __name__ == "__main__":
     main()
