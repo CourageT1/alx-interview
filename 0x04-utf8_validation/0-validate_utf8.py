65002fe5498e21c5a7dd23e51215c9d76ff82933
talent <124376251+CourageT1@users.noreply.github.com>
2024-02-01 09:39:42 +0000

    correcting 1

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -19,19 +19,25 @@ def validUTF8(data):
     for byte in data:
         # Check if the current byte is a leading byte
         if num_bytes == 0:
-            if (byte >> 7) == 0b0:
-                num_bytes = 1
+            if (byte >> 7) == 0:
+                # 1-byte character
+                continue
             elif (byte >> 5) == 0b110:
-                num_bytes = 2
+                # 2-byte character
+                num_bytes = 1
             elif (byte >> 4) == 0b1110:
-                num_bytes = 3
+                # 3-byte character
+                num_bytes = 2
             elif (byte >> 3) == 0b11110:
-                num_bytes = 4
+                # 4-byte character
+                num_bytes = 3
             else:
+                # Invalid leading byte
                 return False
         else:
             # Check if the current byte is a continuation byte
             if (byte >> 6) != 0b10:
+                # Invalid continuation byte
                 return False
 
             num_bytes -= 1
