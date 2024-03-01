d0345bc67d6dba4a250b046f4867e5ccd962d9b7
talent <124376251+CourageT1@users.noreply.github.com>
2024-03-01 15:11:37 +0000

    pycodestyle correction

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -3,6 +3,7 @@
 Module for making change with the fewest number of coins.
 """
 
+
 def makeChange(coins, total):
     """
     Determine the fewest number of coins needed to meet a given total amount.
